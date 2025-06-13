dbecdfb178854022482b16a3610a48b48a6d98e5
Jostin <figueroajostin4@gmail.com>
2025-06-12 22:19:26 -0500

    mensaje

--- ejercicio_6.py
+++ ejercicio_6.py
@@ -1,11 +1,11 @@
 # Calcular la media de tres números pedidos por teclado.
 import statistics
-numero_1 = input ("Ingrese el primer valor:")
+numero_1 = float(input ("Ingrese el primer valor:"))
 
-numero_2 = input ("Ingrese el segundo valor:")
+numero_2 = float(input ("Ingrese el segundo valor:"))
 
-numero_3 = input ("Ingrese el tercer valor:")
+numero_3 = float(input ("Ingrese el tercer valor:"))
 
-media= statistics.mean(numero_1, numero_2, numero_3)
+media= statistics.mean([numero_1, numero_2, numero_3])
 
 print ("La media de los números que ingresó es:", media)
