08ce9a578f1171f704e7f869c6cc8576df87355b
Jostin <figueroajostin4@gmail.com>
2025-06-12 22:31:32 -0500

    primer committ

--- ejercicio_7.py
+++ ejercicio_7.py
@@ -1,2 +1,3 @@
 #Convertir una cantidad de minutos a horas y minutos (ejemplo: 1000 minutos →
 #16 horas y 40 minutos).
+
--- ejercicio_8.py
+++ ejercicio_8.py
@@ -1,1 +1,6 @@
 # Calcular el sueldo total de un vendedor con un sueldo base más un 10% extra por comisión de tres ventas.
+sueldo_base = 450
+comision_por_venta = 0.10
+ventas_totales = int(input("Ingrese las ventas realizadas:"))
+sueldo_total = sueldo_base * (comision_por_venta * ventas_totales)
+print ("El sueldo total que recibirá es de: ", round(sueldo_total,1))
